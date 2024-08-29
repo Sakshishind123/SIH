8db51fb371e37fd66a79bcc2f366d18b66a3ab6c
sakshi_shinde <148954593+Sakshishind123@users.noreply.github.com>
2024-08-29 11:30:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Basic from './components/basic';
 import Login from './components/login'; // Make sure this path is correct
-import SignupPage from './components/signup';
+import SignupPage from './components/sign';
+import Ask from './components/ask'
+import Workerdetails from './components/Workerdetails'
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Basic />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path='/ask' element={< Ask/>} />
+        <Route path="/sign" element={<SignupPage />} />
+        <Route path="/Workerdetails" element={<Workerdetails />} />
       </Routes>
     </Router>
   );
